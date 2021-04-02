6b4cee9734ff3061445c35f1195433c8f4ea67ed
smallSohoSolo <b3069741@126.com>
2021-04-02 18:46:42 +0800

    save

--- build.sh
+++ build.sh
@@ -11,7 +11,7 @@ cp ./local/cargo-config.toml ~/.cargo/config
 cargo build --target armv7-linux-androideabi --release
 
 # optimize
-"$NDK_PATH"/toolchains/arm-linux-androideabi-4.9/prebuilt/darwin-x86_64/bin/arm-linux-androideabi-strip -s target/armv7-linux-androideabi/release/$LIB_NAME
+"$NDK_PATH"/toolchains/arm-linux-androideabi-4.9/prebuilt/darwin-x86_64/bin/arm-linux-androideabi-strip -s target/armv7-linux-androideabi/release/"$LIB_NAME"
 
 # move
 SO_PATH="$PARALLEL_PATH"/src/main/jniLibs/armeabi-v7a
--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,19 @@
 #![cfg(target_os = "android")]
 #![allow(non_snake_case)]
 
+// pub unsafe extern fn Java_com_parallel_android_ParallelEngine_cancel(_env: JNIEnv, _: JObject) {}
+//
+// pub unsafe extern fn Java_com_parallel_android_ParallelEngine_isCanceled(_env: JNIEnv, _: JObject) -> jboolean {}
+//
+// pub unsafe extern fn Java_com_parallel_android_ParallelEngine_isExecuted(_env: JNIEnv, _: JObject) -> jboolean {}
+//
+// pub unsafe extern fn Java_com_parallel_android_ParallelEngine_nativeEnqueue(_env: JNIEnv, _: JObject) {}
+
+
 use jni::JNIEnv;
-use jni::objects::JObject;
-use jni::sys::jstring;
+use jni::objects::{JObject, JValue};
 
 #[no_mangle]
-pub extern fn Java_com_parallel_android_ParallelEngine_cancel(_env: JNIEnv, _: JObject) {}
-
-#[no_mangle]
-pub extern fn Java_com_parallelnet_android_MainActivity_hello(env: JNIEnv, _: JObject) -> jstring {
-    env.new_string("66666").unwrap().into_inner()
+pub unsafe extern fn Java_com_parallelnet_android_MainActivity_test(env: JNIEnv, jobj: JObject) {
+    env.call_method(jobj, "ttt", "(I)V", &[JValue::Object(env.new_string("ttt"))]);
 }
